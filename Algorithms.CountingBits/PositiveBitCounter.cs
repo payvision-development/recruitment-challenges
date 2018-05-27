ed1218c414005aaf94034c9c2e8f8650c1c5679b
Volodya.Gavrysh <Volodya.Gavrysh@gmail.com>
2018-05-27 14:46:11 +0200

    implement PositiveBitCounter.

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -8,12 +8,34 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class PositiveBitCounter
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0)
+                throw new ArgumentException("input should be a zero or a positive number");
+
+            if (input == 0)
+                return new[] { 0 };
+
+            var binaryRepresentation = Convert.ToString(input, 2).Reverse().ToArray();
+
+            var result = new List<int>();
+            for (var i=0; i< binaryRepresentation.Length; i++)
+            {
+                if (binaryRepresentation[i] == '1')
+                {
+                    result.Add(i);
+                }
+            }
+
+            result.Insert(0, result.Count);
+
+            return result;
         }
+
+
     }
 }
