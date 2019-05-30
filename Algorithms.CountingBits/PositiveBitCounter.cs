a007d1279bcc62f087d5bd4a8206ff63532069c4
borken <erice.borja@hotmail.com>
2019-05-30 08:01:52 +0200

    finished counting bit algo

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -13,7 +13,25 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0)
+                throw new ArgumentException("The input value should be positive.");
+            else if (input == 0)
+                return new List<int>() { 0 };
+            else
+            {
+                char[] binaryRepresentation = Convert.ToString(input, 2).ToCharArray();
+                List<int> result = new List<int>();
+                int binaryLength = binaryRepresentation.Length;
+                for (int index = binaryLength - 1; index >= 0; index--)
+                {
+                    if(binaryRepresentation[index] == '1')
+                    {
+                        result.Add(binaryLength - 1 - index);
+                    }
+                }
+                result.Insert(0, result.Count);
+                return result;
+            }            
         }
     }
 }
