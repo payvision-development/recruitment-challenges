98b6dedd3569a133bf52d5533d0816ea80817f20
Sergio López Rico <sergio.lopez@mumablue.com>
2019-10-02 01:57:32 +0200

    Lint errors fixed

--- fraud-detection/src/fraud/rules/address.rule.js
+++ fraud-detection/src/fraud/rules/address.rule.js
@@ -8,4 +8,3 @@ module.exports = function addressFraudRule (current, order) {
     current.creditCard !== order.creditCard
   )
 }
-
--- fraud-detection/src/fraud/rules/email.rule.js
+++ fraud-detection/src/fraud/rules/email.rule.js
@@ -5,4 +5,3 @@ module.exports = function emailFraudRule (current, order) {
     current.creditCard !== order.creditCard
   )
 }
-
