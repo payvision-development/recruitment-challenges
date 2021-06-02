90b1ca0a9937c2b6d1388318c0688aa8c8fb3abe
Froilán <frolesti4@gmail.com>
2021-06-02 07:57:17 +0200

    feat: server & folder structure + view components

--- 1.CastleBlack/app.js
+++ 1.CastleBlack/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const consola = require("consola");
 const router = require("./src/router.js");
 const api = require("./src/api.js");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const host = process.env.HOST || "0.0.0.0";
@@ -18,6 +20,20 @@ async function run() {
   app.use("/", router);
   app.use("/api", api);
 
+  //logs the request results
+  app.use(morgan("tiny"));
+
+  //setting view engine
+  app.set("view engine", "ejs");
+
+  //loading src
+  app.use("/css", express.static(path.resolve(__dirname, "src/css")));
+  app.use("/js", express.static(path.resolve(__dirname, "src/js")));
+  app.use("/img", express.static(path.resolve(__dirname, "src/img")));
+
+  app.get("/", (req, res) => {
+    res.render("index")
+  })
   const server = http.createServer(app);
 
   server.listen(port, host);
